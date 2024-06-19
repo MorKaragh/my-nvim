3b9178e39d0d3e1077cd7be070c2f7ae838940e7
Sergei <hentai.exe@gmail.com>
2024-06-19 22:55:25 +0400

    next

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -30,6 +30,9 @@ return {
             lspconfig.jdtls.setup({
                 capabilities = capabilities,
             })
+            lspconfig.yamlls.setup({
+                capabilities = capabilities,
+            })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
